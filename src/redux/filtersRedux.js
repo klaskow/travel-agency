ab9e1b5a6c93871bd5163245e6aac8bd518bbcb3
Kamil <kam.laskowski@gmail.com>
2019-10-01 21:57:57 +0200

    filters tags, duration

--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -10,21 +10,47 @@ const createActionName = name => `app/${reducerName}/${name}`;
 
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
-// TODO - add other action types
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
+export const ADD_TAG = createActionName('ADD_TAG');
+export const REMOVE_TAG = createActionName('REMOVE_TAG');
 
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
-// TODO - add other action creators
+export const changeDuration = payload => ({ payload, type: CHANGE_DURATION });
+export const addTag = payload => ({ payload, type: ADD_TAG });
+export const removeTag = payload => ({ payload, type: REMOVE_TAG });
 
 // reducer
 export default function reducer(statePart = [], action = {}) {
+  const index = statePart.tags.indexOf(action.payload);
+
   switch (action.type) {
     case CHANGE_PHRASE:
       return {
         ...statePart,
         searchPhrase: action.payload,
       };
-    // TODO - handle other action types
+    case CHANGE_DURATION:
+      return {
+        ...statePart,
+        duration: {
+          ...statePart.duration,
+          ...action.payload,
+        },
+      };
+    case ADD_TAG:
+      return {
+        ...statePart,
+        tags: [...statePart.tags, action.payload],
+      };
+    case REMOVE_TAG:
+      return {
+        ...statePart,
+        tags: [
+          ...statePart.tags.slice(0, index),
+          ...statePart.tags.slice(index + 1),
+        ],
+      };
     default:
       return statePart;
   }
