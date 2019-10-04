25464b3eeb84d7eb3c2050dfa38eeb1ee47a9462
Kamil <kam.laskowski@gmail.com>
2019-10-04 21:02:42 +0200

    calc price

--- src/components/views/Trip/Trip.js
+++ src/components/views/Trip/Trip.js
@@ -10,6 +10,7 @@ import DetailsBox from '../../common/DetailsBox/DetailsBox';
 import DetailsImage from '../../common/DetailsImage/DetailsImage';
 import List from '../../common/List/List';
 import ListItem from '../../common/ListItem/ListItem';
+import OrderForm from '../../features/OrderForm/OrderFormContainer';
 
 import styles from './Trip.scss';
 import { Grid, Row, Col } from 'react-flexbox-grid';
@@ -58,6 +59,7 @@ const Trip = ({
             <Col xs={12}>
               <PageTitle text="Trip details" />
               {HTMLParser(description)}
+              <OrderForm tripCost={cost} />
             </Col>
           </Row>
         </Grid>
