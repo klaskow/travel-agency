c6a2eb054d59d6ecad058c181309a8588d2538b6
Kamil <kam.laskowski@gmail.com>
2019-10-05 14:30:15 +0200

    order option dropdown

---
+++ src/components/features/OrderOption/OrderOptionCheckboxes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OrderOptionCheckboxes = () => {
+  return <div>OrderOptionCheckboxes</div>;
+};
+
+export default OrderOptionCheckboxes;
---
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OrderOptionIcons = () => {
+  return <div>OrderOptionIcons</div>;
+};
+
+export default OrderOptionIcons;
