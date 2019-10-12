505dd1b12b0ceb00664cdf299a56e00d1030ab29
Kamil <kam.laskowski@gmail.com>
2019-10-12 12:41:12 +0200

    test Hero, server.js

---
+++ src/components/layout/Hero/Hero.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Hero from './Hero';
+
+describe('Component Hero', () => {
+  it('should render without crashing', () => {
+    const component = shallow(<Hero titleText="Lorem ipsum" />);
+    expect(component).toBeTruthy();
+  });
+
+  xit('should throw error without required props', () => {
+    expect(() => shallow(<Hero />)).toThrow();
+  });
+});
